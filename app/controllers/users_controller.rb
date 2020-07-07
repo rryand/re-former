2f12d9c46f772a8835c84fa1ac97c822ad154efc
Ramses Ryan Dineros <ramsesryandineros@gmail.com>
2020-07-07 20:08:07 +0800

    added ability to edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    pp User.all
   end
 
   def create
@@ -10,7 +11,21 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
-      render :new
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
     end
   end
 
